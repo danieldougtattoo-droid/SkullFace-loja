22bdf99d4746f8e61c7c345735a2aa73dcdde536
Doug Prog <danieldougtattoo@gmail.com>
2026-03-03 14:06:19 -0300

    Configurando PostgreSQL com SSL para produção

--- studio/settings.py
+++ studio/settings.py
@@ -115,7 +115,10 @@ WSGI_APPLICATION = 'studio.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'),
+    conn_max_age=600,
+    ssl_require=True,
+    )
    
 }
 
